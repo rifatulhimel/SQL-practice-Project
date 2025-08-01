10fe6da3858e23bfe0bed991b03e15228e99cbaf
rifatulhimel <162656878+rifatulhimel@users.noreply.github.com>
2025-08-01 00:44:05 -0600

    Add files via upload

---
+++ change over trend.sql
@@ -0,0 +1,32 @@
+SELECT
+YEAR(order_date) as order_year,
+MONTH(order_date) as order_year,
+SUM(sales_amount) as total_Sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date)
+
+
+SELECT
+DATETRUNC(year, order_date) as order_date,
+SUM(sales_amount) as total_Sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(year, order_date)
+ORDER BY DATETRUNC(year, order_date)
+
+
+SELECT
+FORMAT(order_date, 'yyyy-MMM') as order_date,
+SUM(sales_amount) as total_Sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
